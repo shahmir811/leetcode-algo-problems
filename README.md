786e92a945c26a6357e4b7361b71d6089f7dd109
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-06-21 10:36:21 +0500

    Added solution for MaximumSubarray problem

--- README.md
+++ README.md
@@ -32,3 +32,15 @@ For real time testing, run the following command inside folder:
 ```bash
 jest problem/test --watch
 ```
+
+## Leetcode Problems List
+
+1 - Easy
+
+- 01 [Two Sum](https://leetcode.com/problems/two-sum/description/)
+- 14 [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)
+- 53 [Maximum Subarray](https://leetcode.com/problems/maximum-subarray/description/)
+
+2 - Medium
+
+- 02 [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
---
+++ easy/53_MaximumSubarray/index.js
@@ -0,0 +1,36 @@
+// Problem link:
+// https://leetcode.com/problems/maximum-subarray/description/
+
+// STATEMENT
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum
+// and return its sum.
+// A subarray is a contiguous part of an array.
+
+// REQUIREMENTS
+// -
+// -
+
+// EXAMPLES
+
+// Example 1:
+// Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// Output: 6;
+
+// Example 2:
+// Input: nums = [1];
+// Output: 1;
+
+// Example 3:
+// Input: nums = [5, 4, -1, 7, 8];
+// Output: 23;
+
+const maxSubArray = nums => {
+	let solution = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+		solution = Math.max(solution, nums[i]);
+	}
+
+	return solution;
+};
